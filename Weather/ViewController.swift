f0e830a49402e801377878356238e990b4f46c81
Nikita Andreev <nikita.andreev.work@gmail.com>
2020-11-04 10:45:34 +0200

    Researching methods

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -16,5 +16,5 @@ class ViewController: UIViewController {
     }
 
 
-} // Methods - The Self Property [after finishing methods can carry on with the clip (CLIP 31:00)
+} // Methods - Type Methods [after finishing methods can carry on with the clip (CLIP 31:00)
 
