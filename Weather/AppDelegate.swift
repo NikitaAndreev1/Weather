1d994ba9b09f799b177f33c6c08175d1dfcfb60e
Nikita Andreev <nikita.andreev.work@gmail.com>
2020-12-03 11:01:16 +0800

    Learning autolayout

--- Weather/AppDelegate.swift
+++ Weather/AppDelegate.swift
@@ -12,11 +12,13 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
+    var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        // Override point for customization after application launch.
+//        return true
+//    }
 }
 
--- Weather/LoginFormController.swift
+++ Weather/LoginFormController.swift
@@ -1,29 +1,26 @@
 //
-//  ViewController.swift
+//  LoginFormController.swift
 //  Weather
 //
-//  Created by Nikita on 30.10.2020.
+//  Created by Nikita on 01.12.2020.
 //  Copyright © 2020 Nicks. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginFormController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
-} // (CLIP 2:53:00)
+}
+// (CLIP 2:53:00)
 
 //UIWindow - a window
 //UIViewController - controller - shows view - screen
 //UIView - graphical element
-//Main.storyboard is a graphical sketch of an interface, but not an interface itself 
+//Main.storyboard is a graphical sketch of an interface, but not an interface itself
 
 
 // Learning Subscript - Subscript Options 
-
-
