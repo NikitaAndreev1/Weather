83714276494d564e82969cbc94af032394745220
Nikita Andreev <nikita.andreev.work@gmail.com>
2020-12-10 17:23:01 +0800

    Working on login in interface

--- Weather/LoginFormController.swift
+++ Weather/LoginFormController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class LoginFormController: UIViewController {
 
+    @IBOutlet weak var titleView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
